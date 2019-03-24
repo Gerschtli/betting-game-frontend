08a25ccb6aaa19d6e5852532bfeb74a91267503d
Tobias Happ <tobias.happ@gmx.de>
2019-03-24 14:02:12 +0100

    Add font-awesome icons

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faBars);
+library.add(faTimes);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
