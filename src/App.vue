4243a333df546f2ef507373c718dd81e0b9bc9ef
Tobias Happ <tobias.happ@gmx.de>
2019-03-24 14:04:02 +0100

    Add header component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <template lang="pug">
   #app
-    #nav
-      router-link(to="/") Home
-      router-link(to="/about") About
+    Header
     router-view
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Header from '@/components/Header.vue';
+
+@Component({
+  components: {
+    Header,
+  },
+})
+export default class App extends Vue {}
+</script>
+
 <style lang="sass">
-#app
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
+\:root
+  --color-text-dark: #182628
+  --color-background-accent: #65CCB8
+  --color-accent-light: #57BA98
+  --color-accent: #3B945E
+  --color-background: #F2F2F2
+  --color-overlay: rgba(0, 0, 0, .3)
+
+  --font-family: 'Roboto', sans-serif
+
+html
+  box-sizing: border-box
+
+*, *:before, *:after
+  box-sizing: inherit
+
+body
+  margin: 0
+  padding: 0
+  font-family: var(--font-family)
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
-
-#nav
-  padding: 30px
-
-  a
-    margin-right: .5em
-    font-weight: bold
-    color: #2c3e50
-
-    &:last-child
-      margin-right: 0
-
-    &.router-link-exact-active
-      color: #42b983
 </style>
