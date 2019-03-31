1764628eb9c5e71090700e98691a248beb5de354
Tobias Happ <tobias.happ@gmx.de>
2019-03-31 15:11:08 +0200

    Add example markup for App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template lang="pug">
-  #app
-    router-view
+  v-app
+    v-toolbar(app)
+      v-toolbar-title(class="headline text-uppercase")
+        span Vuetify
+        span(class="font-weight-light") MATERIAL DESIGN
+      v-spacer
+      v-btn(
+        flat,
+        href="https://github.com/vuetifyjs/vuetify/releases/latest",
+        target="_blank"
+      )
+        span(class="mr-2") Latest Release
+
+    v-content
+      router-view
 </template>
 
 <script lang="ts">
