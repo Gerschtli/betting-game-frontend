f60e97fafb1ce96b2e06be77ebc6c92269288f30
Tobias Happ <tobias.happ@gmx.de>
2019-04-19 21:38:35 +0200

    Always set Authorization header if accessToken is available

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -2,19 +2,21 @@ import axios from 'axios';
 import { BAD_REQUEST, UNAUTHORIZED, UNPROCESSABLE_ENTITY } from 'http-status-codes';
 
 import { ApiError, BadRequest, Unauthorized, UnprocessableEntity } from '@/errors';
+import store from '@/store';
 
 axios.defaults.baseURL = 'http://192.168.56.101:5000';
 
-// axios.interceptors.request.use(
-//   (config) => {
-//     const token = store.getters.token;
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   Promise.reject,
-// );
+axios.interceptors.request.use(
+  (config) => {
+    const accessToken = store.getters['authentication/accessToken'];
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  Promise.reject,
+);
 
 axios.interceptors.response.use(
   (response) => response,
