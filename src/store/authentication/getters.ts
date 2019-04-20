19c8f8b2400504d5d54fab8fa54901a8105fcf9b
Tobias Happ <tobias.happ@gmx.de>
2019-04-20 14:45:47 +0200

    Add isAuthenticated getter

--- src/store/authentication/getters.ts
+++ src/store/authentication/getters.ts
@@ -3,9 +3,14 @@ import { AuthenticationState } from './types';
 import { RootState } from '../types';
 
 export const ACCESS_TOKEN = 'ACCESS_TOKEN';
+export const IS_AUTHENTICATED = 'IS_AUTHENTICATED';
 
 export const getters: GetterTree<AuthenticationState, RootState> = {
   [ACCESS_TOKEN](state: AuthenticationState): string | undefined {
     return state.accessToken;
+  },
+
+  [IS_AUTHENTICATED](state: AuthenticationState): boolean {
+    return state.accessToken !== undefined;
   },
 };
