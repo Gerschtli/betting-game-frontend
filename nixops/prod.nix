5a45c8d99f95bed41c39d6315c019dfbbc6e110f
Tobias Happ <tobias.happ@gmx.de>
2019-04-20 15:31:54 +0200

    Fix browser cache for important files

--- nixops/prod.nix
+++ nixops/prod.nix
@@ -29,7 +29,25 @@ in
 
           virtualHosts."betting-game-frontend" = {
             root = import ../.;
-            locations."/".tryFiles = "$uri /index.html";
+            locations = {
+              "/".tryFiles = "$uri /index.html";
+
+              "~* (?:\.(?:html|json)|service-worker\.js)$".extraConfig = ''
+                expires -1;
+              '';
+
+              "~* \.(?:jpg|jpeg|gif|png|ico|cur|gz|svg|svgz|mp4|ogg|ogv|webm|htc)$".extraConfig = ''
+                expires 1M;
+                access_log off;
+                add_header Cache-Control "public";
+              '';
+
+              "~* \.(?:css|js)$".extraConfig = ''
+                expires 1y;
+                access_log off;
+                add_header Cache-Control "public";
+              '';
+            };
           };
         };
       }
