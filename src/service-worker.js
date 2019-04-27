915021e86335a6254bd4865114bb205c920e9c49
Tobias Happ <tobias.happ@gmx.de>
2019-04-27 23:58:20 +0200

    Use workbox InjectManifest option

---
+++ src/service-worker.js
@@ -0,0 +1,11 @@
+workbox.core.setCacheNameDetails({ prefix: "betting-game-frontend" });
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   pwa: {
     name: 'Fußball Tippspiel',
-    themeColor: '#78909C'
+    themeColor: '#78909C',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/service-worker.js',
+    },
   }
 }
