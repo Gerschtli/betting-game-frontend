382d7a7f8935486b57707fad4590454be5fc2bf9
Tobias Happ <tobias.happ@gmx.de>
2019-05-02 23:44:32 +0200

    Fix nginx cache headers

--- nixops/prod.nix
+++ nixops/prod.nix
@@ -33,19 +33,24 @@ in
               "/".tryFiles = "$uri /index.html";
 
               "~* (?:\.(?:html|json)|service-worker\.js)$".extraConfig = ''
-                expires -1;
+                add_header Cache-Control 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0';
+                expires off;
+                proxy_no_cache 1;
+              '';
+
+              "^~ /img/icons/".extraConfig = ''
+                add_header Cache-Control 'public, must-revalidate';
+                expires 1M;
               '';
 
               "~* \.(?:jpg|jpeg|gif|png|ico|cur|gz|svg|svgz|mp4|ogg|ogv|webm|htc)$".extraConfig = ''
-                expires 1M;
-                access_log off;
                 add_header Cache-Control "public";
+                expires 1M;
               '';
 
               "~* \.(?:css|js)$".extraConfig = ''
-                expires 1y;
-                access_log off;
                 add_header Cache-Control "public";
+                expires 1y;
               '';
             };
           };
