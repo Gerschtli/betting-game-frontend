0b8a0ce98f092961a6bc76a9625f11524760d972
Tobias Happ <tobias.happ@gmx.de>
2019-07-14 23:04:24 +0200

    Add shellHook to link node and npm bins

--- shell.nix
+++ shell.nix
@@ -1,17 +1,22 @@
 let
   pkgs = import ./nixops/nixpkgs.nix;
   app = import ./nixops/app.nix;
+  nodejs = app.nodejs pkgs;
 in
 
 pkgs.mkShell {
   name = "betting-game-frontend";
 
   buildInputs = [
-    (app.nodejs pkgs)
+    nodejs
     pkgs.git-crypt
   ];
 
   shellHook = ''
     export PATH="$PWD/node_modules/.bin:$PATH"
+
+    mkdir -p .env-bins
+    ln -snf ${nodejs}/bin/node .env-bins/node
+    ln -snf ${nodejs}/bin/npm .env-bins/npm
   '';
 }
